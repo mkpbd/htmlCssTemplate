8e780c0a6a8af25a29c3f17efd5f66dcd39762aa
mkpbd <mostofakamal1990@gmail.com>
2023-08-30 14:50:20 +0600

    css update theme

--- cv-template.js
+++ cv-template.js
@@ -6,8 +6,6 @@ let imageChange = document.getElementById('image-change');
 
 let ulli = document.querySelectorAll('ul li');
 
-const editButton = document.querySelector('.mkp__card');
-
 // Get the modal
 let modal = document.getElementById("myModal");
 // Get the <span> element that closes the modal
@@ -15,7 +13,8 @@ let span = document.getElementsByClassName("close")[0];
 
 uploadImage.addEventListener('click', function(event){
     modal.style.display = 'block'
-    console.log(uploadImage);
+    console.log(uploadImage)
+
 });
 
 const imageArray = [];
@@ -47,18 +46,12 @@ span.addEventListener("click", function(event){
 
 editable.forEach(function(elem){
 
-    elem.addEventListener('mouseenter', function(ev){
+    elem.addEventListener('click', function(ev){
         ev.preventDefault();
         // console.log(elem.innerHTML);
-        //  elem.setAttribute('contenteditable', 'true');
-         ev.target.setAttribute('contenteditable', 'true');
-        ev.target.style.border = '1px solid #ddd';
-        ev.target.padding ='none';
-
-        editButton.style.top = (ev.layerY)+'px';
-        editButton.style.display = 'block';
-      
-        console.log(ev);        // let anchor = ev.tagret.document.querySelector('a');
+        // elem.setAttribute('contenteditable', 'true');
+        console.log(ev.target.setAttribute('contenteditable',true));
+        // let anchor = ev.tagret.document.querySelector('a');
         // anchor.setAttribute('contenteditable', 'true');
         // anchor.addEventListener('input', function(e){
 
@@ -66,46 +59,6 @@ editable.forEach(function(elem){
        // });
     });
 })
-
-editable.forEach(function(elem){
-
-  elem.addEventListener('click', function(ev){
-      ev.preventDefault();
-      // console.log(elem.innerHTML);
-      //  elem.setAttribute('contenteditable', 'true');
-       ev.target.setAttribute('contenteditable', 'true');
-      ev.target.style.border = '1px solid #ddd';
-      ev.target.padding ='none';
-
-      editButton.style.top = (ev.layerY)+'px';
-      editButton.style.display = 'block';
-    
-      console.log(ev);        // let anchor = ev.tagret.document.querySelector('a');
-      // anchor.setAttribute('contenteditable', 'true');
-      // anchor.addEventListener('input', function(e){
-
-        
-     // });
-  });
-})
-
-// mouse leave event
-
-editable.forEach(function(elem){
-
-  elem.addEventListener('mouseout', function(ev){
-      ev.preventDefault();
-  
-      ev.target.setAttribute('contenteditable',false);
-      ev.target.style.border = 'none';
-      ev.target.style.padding = '0px';
-      editButton.style.display = 'none';
-        
-     // });
-  });
-})
-
-
 
 // p.forEach(function(elem){
 
@@ -127,6 +80,27 @@ editable.forEach(function(elem){
 
 //  show button on mouse click 
 
+
+const editButton = document.querySelector('.mkp__card');
+
+
+
+editable.forEach(element => {
+  // console.log('foreach is running');
+  let isEventTrue = false;
+   element.addEventListener('mouseenter', ev =>{
+
+    editButton.style.top = (ev.layerY+10)+'px';
+    editButton.style.display = 'block';
+    // 
+    //element.after(`10283`)
+   //  element.innerText = 'hello kamal';
+
+   
+   // console.log(ev);
+   })
+});
+
 // p.forEach(element => {
 //   // console.log('foreach is running');
 //   let isEventTrue = false;
@@ -146,25 +120,20 @@ editable.forEach(function(elem){
 
 // edtibale Button 
 
-//const editiableButtonForelementEdit = document.getElementById('edit-section');
+const editiableButtonForelementEdit = document.getElementById('edit-section');
 
-// editiableButtonForelementEdit.addEventListener('click', function(e){
+editiableButtonForelementEdit.addEventListener('click', function(){
+  editable.forEach(function(elem){
 
-//   // console.log(e);
-//   editable.forEach(function(elem){
-
-//     elem.addEventListener('input', function(ev){
-//       ev.target.setAttribute('contenteditable',true);
-//       console.log(ev.target);
-//       elem.style.border= '2px solid red';
-//       elem.style.padding = '5px 10px';
-//       elem.style.margin = '10px 10px';
-//     });
-//     //  console.log(elem);
-//     // elem.target.setAttribute('contenteditable',true);
-//     // elem.style.border= '2px solid red';
-//     // elem.style.padding = '5px 10px';
-//     // elem.style.margin = '10px 10px';
-//    })
-// // })
+  
+    // elem.target.setAttribute('contenteditable',true);
+    elem.style.border= '2px solid red';
+    elem.style.padding = '5px 10px';
+    elem.style.margin = '10px 10px';
+   
+           
+        
+   
+   })
+})
 
